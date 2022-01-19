99fbb7d0162a27113bdec13321a7cdab4bc23efa
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2022-01-19 21:58:39 +0300

    Initial Commit

---
+++ EduardMudritskiiHW 3.1/EduardMudritskiiHW_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  EduardMudritskiiHW_3_1App.swift
+//  EduardMudritskiiHW 3.1
+//
+//  Created by Эдуард on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct EduardMudritskiiHW_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
